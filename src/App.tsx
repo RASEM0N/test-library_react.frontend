754d75b4a18d3d332da1fed3df87aee8f72425ae
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-06-25 20:03:18 +0500

    тест для хука useForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from 'react'
+import TextField from './components/Field/TextField'
+import { useForm } from './hooks'
+
+interface FormDataType {
+    title: string
+    author: string
+}
 
 const App = () => {
-  return <div>App</div>;
-};
+    const { error, handleChange, handleSubmit, values } = useForm<FormDataType>({
+        initialValues: {
+            author: '',
+            title: '',
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values))
+        },
+    })
 
-export default App;
+    return (
+        <div className="wrapper">
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    name="author"
+                    label="Author"
+                    value={values.author}
+                    onChange={handleChange}
+                    error={error.author}
+                />
+                <TextField
+                    name="title"
+                    label="Title"
+                    value={values.title}
+                    onChange={handleChange}
+                    error={error.title}
+                />
+                <button type="submit">submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default App
