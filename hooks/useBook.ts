154a6eb85ad3a8275e49a663e5466a904057df95
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-06-25 17:19:27 +0500

    create: useBook + other

---
+++ hooks/useBook.ts
@@ -0,0 +1,57 @@
+import useLocalStorage from './useLocalStorage'
+import { useCallback, useMemo } from 'react'
+import { Book, FormDataBook } from '../types'
+import { booksToString, stringToBooks } from '../utils/dataNormalization'
+import { generationID } from '../utils/generation'
+
+const useBook = () => {
+    const { item, setItem, error } = useLocalStorage('books', [])
+
+    const books = useMemo<Book[]>(() => {
+        return stringToBooks(item)
+    }, [item])
+
+    const change = (books: Book[]): void => {
+        const value: string = booksToString(books)
+        setItem(value)
+    }
+
+    // ---- ----- CREATE ----- ----
+    const createBook = useCallback((formData: FormDataBook): void => {
+        const createBook: Book = {
+            ...formData,
+            _id: generationID(),
+        }
+        change([...books, createBook])
+    }, [])
+
+    // ---- ----- UPDATE ----- ----
+    const updateBook = useCallback((formData: FormDataBook, bookId: string): void => {
+        const updatedBooks = books.map((b) => {
+            if (b._id === bookId) {
+                return {
+                    ...b,
+                    ...formData,
+                }
+            }
+            return b
+        })
+        change(updatedBooks)
+    }, [])
+
+    // ---- ----- DELETE ----- ----
+    const deleteBook = useCallback((bookId: string): void => {
+        const updatedBooks = books.filter((b) => b._id !== bookId)
+        change(updatedBooks)
+    }, [])
+
+    return {
+        error,
+        books,
+        createBook,
+        updateBook,
+        deleteBook,
+    }
+}
+
+export default useBook
---
+++ utils/dataNormalization.ts
@@ -0,0 +1,22 @@
+import { Book } from '../types'
+
+export const stringToBooks = (value: string): Book[] => {
+    if (typeof value === 'undefined') {
+        return []
+    }
+    try {
+        const books = JSON.parse(value)
+        if (books instanceof Array) {
+            // глубокая проверка?
+            return books as Book[]
+        }
+        return []
+    } catch (error) {
+        console.log(error.message)
+        return []
+    }
+}
+
+export const booksToString = (books: Book[]): string => {
+    return JSON.stringify(books)
+}
